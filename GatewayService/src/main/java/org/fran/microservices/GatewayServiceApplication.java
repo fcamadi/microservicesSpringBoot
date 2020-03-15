a5b5eda75565282add0f5d2ab80bbf73d66a32d0
fcamadi <fcamadi@gmail.com>
2020-03-15 18:34:11 +0100

    GatewayService - initial commit

---
+++ GatewayService/src/main/java/org/fran/microservices/GatewayServiceApplication.java
@@ -0,0 +1,23 @@
+package org.fran.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 
+ *  @EnableDiscoveryClient
+ *  To register within the Eureka service
+ * 
+ * @author francd
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
