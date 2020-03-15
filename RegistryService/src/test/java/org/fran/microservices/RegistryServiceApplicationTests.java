91eb640333000e66ba255ff4917c411fbe028b21
fcamadi <fcamadi@gmail.com>
2020-03-15 18:18:05 +0100

    RegistryService - initial commit

---
+++ RegistryService/src/test/java/org/fran/microservices/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fran.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
